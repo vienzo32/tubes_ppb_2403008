e495c58430c0a4dc6f8cf4b6e05191a80f8bea14
Davi D3TI2B <daviernantamaulana@gmail.com>
2026-06-10 08:43:34 +0700

    provider

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../utils/shared_pref_helper.dart';
+import '../services/api_service.dart';
 
 class AuthState {
   final bool isLoggedIn;
@@ -30,3 +31,36 @@ class AuthState {
 final authProvider = StateProvider<AuthState>((ref) {
   return AuthState.initial();
 });
+
+Future<bool> login(WidgetRef ref, String email, String password) async {
+  final result = await ApiService.login(email, password);
+  
+  if (result['success'] == true) {
+    final user = result['user'];
+    final userId = user['id'];
+    final userEmail = user['email'];
+    final userName = user['name'];
+    final userRole = user['role'];
+    
+    await SharedPrefHelper.setLoggedIn(true);
+    await SharedPrefHelper.setUserId(userId);
+    await SharedPrefHelper.setUserEmail(userEmail);
+    await SharedPrefHelper.setUserName(userName);
+    await SharedPrefHelper.setUserRole(userRole);
+
+    ref.read(authProvider.notifier).state = AuthState(
+      isLoggedIn: true,
+      userId: userId,
+      userEmail: userEmail,
+      userName: userName,
+      userRole: userRole,
+    );
+    return true;
+  }
+  return false;
+}
+
+Future<void> logout(WidgetRef ref) async {
+  await SharedPrefHelper.logout();
+  ref.read(authProvider.notifier).state = AuthState.initial();
+}
